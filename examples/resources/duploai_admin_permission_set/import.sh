57708b5990b08df9d834b7867200184821832bdd
Duplo-nikv <158246285+Duplo-nikv@users.noreply.github.com>
2026-06-22 16:08:28 +0530

    TF - duplo-ai-admin - duploai_admin_permission_set (#88)
    * permission set resource implementation
    
    * add min item to allowed workspace

---
+++ examples/resources/duploai_admin_permission_set/import.sh
@@ -0,0 +1,5 @@
+# Import an existing permission set.
+#  - PERMISSION_SET_ID is the unique identifier of the permission set (e.g. 6a2258e94703bc957a1b824e)
+terraform import duploai_admin_permission_set.mypermset PERMISSION_SET_ID
+# Example:
+# terraform import duploai_admin_permission_set.mypermset 6a2258e94703bc957a1b824e
