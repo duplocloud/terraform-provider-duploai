fa74ff29fb53cbb92771c1c12177680a398bf9b2
loveduplo <love.purohit@duplocloud.net>
2026-08-28 14:55:33 +0530

    Add cluster autoscaler, disk encryption, volumes, tags to node group
    ClickUp: CUST-11931

--- examples/resources/duploai_node_group/resource.tf
+++ examples/resources/duploai_node_group/resource.tf
@@ -6,10 +6,11 @@ resource "duploai_node_group" "basic" {
   resource_group_id = "<eks-resource-group-id>"
   environment_id    = "<environment-id>"
 
-  instance_types = ["t3.medium"]
-  min_size       = 1
-  max_size       = 3
-  desired_size   = 2
+  instance_types            = ["t3.medium"]
+  min_size                  = 1
+  max_size                  = 3
+  desired_size              = 2
+  enable_cluster_autoscaler = true
 }
 
 # Spot node group with disk sizing, labels, and a taint
@@ -25,6 +26,7 @@ resource "duploai_node_group" "spot" {
   max_size       = 10
   desired_size   = 2
   disk_size_gb   = 100
+  kms_key_id     = "<kms-key-arn>"
   capacity_type  = "<capacity-type>"
   ami_type       = "<ami-type>"
 
@@ -39,6 +41,18 @@ resource "duploai_node_group" "spot" {
       effect = "<taint-effect>"
     }
   ]
+
+  volumes = [
+    {
+      device_name    = "/dev/xvdb"
+      volume_size_gb = 100
+      volume_type    = "gp3"
+    }
+  ]
+
+  tags = {
+    team = "data-platform"
+  }
 
   allocation_tag = "batch"
 
